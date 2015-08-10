074e67041649674c094b2780bceed9b7d40f2dc2
slicn <silicn@126.com>
2015-08-10 18:38:29 +0800

    Initial Commit

---
+++ SwiftCustom/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftCustom
+//
+//  Created by jiahao on 15/8/10.
+//  Copyright (c) 2015年 shiguanghui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
